fb8e6fde70af5142fc7275e02d0a61de4fe4b14e
Ash <ashlandmwest@gmail.com>
2021-12-23 23:24:03 -0800

    Add some starting buttons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Recipe Finder</h1>
+        <button id="edit-pantry">Edit Pantry</button>
+        <button id="search-for-recipies">Search for recipies</button>
       </header>
     </div>
   );
