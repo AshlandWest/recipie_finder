c0fb2e8686205ba5d040a9034ae29d063bb26ba7
Ash <ashlandmwest@gmail.com>
2021-12-23 23:46:39 -0800

    Remove default react styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { API_KEY } from "../env";
+import { API_KEY } from "./env";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Recipe Finder</h1>
-        <button id="edit-pantry">Edit Pantry</button>
-        <button id="search-for-recipies">Search for recipies</button>
-        <p>{API_KEY}</p>
-      </header>
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+
+const Main = () => {
+  return (
+    <main>
+      <h1>Recipe Finder</h1>
+      <button id="edit-pantry">Edit Pantry</button>
+      <button id="search-for-recipies">Search for recipies</button>
+    </main>
+  );
+};
+
+export default Main;
