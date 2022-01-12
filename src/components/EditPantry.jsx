5e4408d8968de57edaf5bdcb98f24c775b90a1cd
Ash <ashlandmwest@gmail.com>
2022-01-12 02:25:46 -0800

    Set up local storage

--- src/components/EditPantry.jsx
+++ src/components/EditPantry.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const EditPantry = () => {
-  return <h1>Edit Pantry</h1>;
+  const oldStorage = localStorage.getItem("ingredients")
+    ? JSON.parse(localStorage.getItem("ingredients"))
+    : [];
+  console.log(oldStorage);
+  const [storage, setStorage] = useState(oldStorage);
+
+  const addItem = () => {
+    const ingrdntField = document.getElementById("ingrdntField");
+    if (ingrdntField.value) {
+      const output = [...storage];
+      output.push(ingrdntField.value);
+      setStorage(output);
+    }
+  };
+
+  useEffect(() => {
+    localStorage.setItem("ingredients", JSON.stringify(storage));
+  }, [storage]);
+
+  return (
+    <div id="edit-pantry">
+      <h1>Edit Pantry</h1>
+      <fieldset>
+        <legend>Ingredients</legend>
+        <input type="text" id="ingrdntField" />
+        <br />
+        <button id="addItemButton" onClick={addItem}>
+          Add Item
+        </button>
+      </fieldset>
+
+      <div>{storage.join(", ")}</div>
+    </div>
+  );
 };
 
 export default EditPantry;
