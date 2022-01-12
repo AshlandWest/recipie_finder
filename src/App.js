8e623d1bba0b6900b2e5c1b075fa435a0865317a
Ash <ashlandmwest@gmail.com>
2022-01-11 16:49:13 -0800

    Add navbar'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Main from "./components/Main";
+import TopBar from "./components/TopBar";
 
 function App() {
   return (
     <div className="App">
+      <TopBar />
       <Main />
     </div>
   );
---
+++ src/components/TopBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TopBar = () => {
+  return <div id="top-bar">I am a Nav Bar</div>;
+};
+
+export default TopBar;
